e2170cac5de0379213e894a31225ee8fa86ae55a
Hirokazu Fukami <fkm@mokelab.com>
2020-09-03 16:55:13 +0900

    Implement game over

--- wasm/game/engine.go
+++ wasm/game/engine.go
@@ -9,8 +9,18 @@ type Engine interface {
 	AddImage(key string, image js.Value)
 	AddEnemy(enemy *gameObject)
 	Player() *gameObject
+
+	Miss() bool
+	ToGameOver()
 	DoFrame(key int16, ctx js.Value)
 }
+
+const (
+	gameStateMain     gameState = 1
+	gameStateGameOver gameState = 2
+)
+
+type gameState int
 
 type engine struct {
 	images        map[string]js.Value
@@ -18,6 +28,8 @@ type engine struct {
 	enemies       []*gameObject
 	hiddenEnemies []*gameObject
 	stage         *gameObject
+	gameState     gameState
+	life          int
 }
 
 // New creates engine instance
@@ -30,6 +42,8 @@ func New() Engine {
 		enemies:       make([]*gameObject, 0, 100),
 		hiddenEnemies: make([]*gameObject, 0, 100),
 		stage:         stg,
+		gameState:     gameStateMain,
+		life:          3,
 	}
 }
 
@@ -45,21 +59,57 @@ func (e *engine) Player() *gameObject {
 	return e.player
 }
 
+func (e *engine) Miss() bool {
+	e.life--
+	return e.life > 0
+}
+
+func (e *engine) ToGameOver() {
+	e.gameState = gameStateGameOver
+}
+
 func (e *engine) DoFrame(key int16, ctx js.Value) {
-	movePlayer(e.player, key)
+	if e.gameState == gameStateMain {
+		e.DoMainFrame(key, ctx)
+		return
+	}
+}
+
+func (e *engine) DoMainFrame(key int16, ctx js.Value) {
+	movePlayer(e.player, key, e)
 	moveEnemy(e.enemies, e)
 	moveEnemy(e.hiddenEnemies, e)
+	hitCheck(e.player, e.enemies)
 	e.stage.moveFunc(e.stage, e)
 
 	ctx.Call("clearRect", 0, 0, 320, 480)
 	drawEnemy(ctx, e.images, e.enemies)
-	ctx.Call("drawImage", e.images["player"], e.player.x, e.player.y)
+	drawPlayer(ctx, e.images, e.player)
 
 	e.enemies = pack(e.enemies)
 	e.hiddenEnemies = pack(e.hiddenEnemies)
 }
 
-func movePlayer(player *gameObject, key int16) {
+func movePlayer(player *gameObject, key int16, engine Engine) {
+	if !player.alive {
+		player.frame--
+		if player.frame == 0 {
+			// game over check
+			if engine.Miss() {
+				player.alive = true
+				player.frame = 120
+				player.x = 160
+				player.y = 440
+			} else {
+				engine.ToGameOver()
+			}
+		}
+		return
+	}
+	if player.frame > 0 {
+		// blink frame
+		player.frame--
+	}
 	if key&1 != 0 {
 		player.y -= 4
 	}
@@ -82,9 +132,36 @@ func moveEnemy(enemies []*gameObject, engine Engine) {
 	}
 }
 
+func hitCheck(player *gameObject, enemies []*gameObject) {
+	if !player.alive {
+		return
+	}
+	if player.frame > 0 {
+		return
+	}
+	for _, e := range enemies {
+		if isHit(player, e) {
+			player.alive = false
+			player.frame = 60
+			break
+		}
+	}
+}
+
 func drawEnemy(ctx js.Value, images map[string]js.Value, enemies []*gameObject) {
 	for _, e := range enemies {
-		ctx.Call("drawImage", images["player"], e.x, e.y)
+		ctx.Call("drawImage", images["player"], e.x-12, e.y-12)
+	}
+}
+
+func drawPlayer(ctx js.Value, images map[string]js.Value, player *gameObject) {
+	if player.alive {
+		if player.frame%3 == 1 {
+			// blink
+			return
+		}
+		ctx.Call("drawImage", images["player"], player.x-12, player.y-12)
+		return
 	}
 }
 
--- wasm/game/game_object.go
+++ wasm/game/game_object.go
@@ -1,5 +1,7 @@
 package game
 
+import "math"
+
 const (
 	objTypePlayer     = 1
 	objTypePlayerShot = 2
@@ -29,4 +31,11 @@ func newObject(objType int, x, y float64) *gameObject {
 		alive:    true,
 		moveFunc: nil,
 	}
+}
+
+func isHit(obj1 *gameObject, obj2 *gameObject) bool {
+	xDiff := obj1.x - obj2.x
+	yDiff := obj1.y - obj2.y
+	dist := math.Sqrt(xDiff*xDiff + yDiff*yDiff)
+	return dist < 4
 }
